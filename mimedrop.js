e2a4931365eb0e08b10dbde36fa4b6c689bf9230
Jan Posselt <github@janposselt.de>
2011-06-26 04:26:40 +0200

    updated demo index, some cleanup, made handle call available throug plugin api

--- mimedrop.js
+++ mimedrop.js
@@ -1,71 +1,68 @@
-(function( $ ){
+(function($) {
     var s;
     // Methods
-    var m = {
-        init: function(e){},
-        start: function(e){},
-        traverse: function(files,area){
-            if (typeof files !== "undefined") {
-                for (var i=0, l=files.length; i<l; i++) {
-                    m.handle(files[i], area, i);
-                }
-            } else {
-                area.html(s.nosupport);
-            }
+    var methods = {
+        init: function(o) {
+            s = {
+                'nosupport'   : 'No support for the File API in this web browser',
+                'maxsize'     : '500000' //Kb
+            };
+            return this.each(function() {
+                if (o) $.extend(s, o);
+
+                $(this)
+                        .bind({
+                            dragleave: function (e) {
+                                e.preventDefault();
+                                $(this).removeClass('over');
+                            },
+                            dragenter: function (e) {
+                                e.preventDefault();
+                                $(this).addClass('over');
+                            },
+                            dragover: function (e) {
+                                e.preventDefault();
+                            }
+                        });
+                this.addEventListener("drop", function (e) {
+                    e.preventDefault();
+                    $(this).removeClass('over');
+
+                    if (typeof e.dataTransfer.files !== "undefined") {
+                        for (var i = 0, l = e.dataTransfer.files.length; i < l; i++) {
+                            methods.handle.call(this, e.dataTransfer.files[i], i);
+                        }
+                    } else {
+                        $(this).html(s.nosupport);
+                    }
+                }, false);
+            });
         },
-        handle: function(file, area, index) {
+        handle: function(file, index, mimetype) {
+            var nextType = undefined;
+            var $this = this;
+
+            var type = (mimetype ? mimetype : file.type),
+                    type = type ? type : 'application/octet-stream';
+
             $.each(s.handler, function() {
-                var type = file.type ? file.type : 'application/octet-stream';
                 if (type.match(new RegExp(this[0].replace(/\*/g, '.*?').replace(/\+/, '\\+')))) {
-                    this[1].call(area, file, index);
+                    this[1].call($this, file, index);
                     return false;
                 }
             });
 
+            return $(this);
         }
     };
-    $.fn.mimedrop = function(o) {
-        // Settings
-        s = {
-            'init': m.init,
-            'start': m.start,
-            'instructions': 'drop a file here',
-            'over'        : 'drop file here!',
-            'nosupport'   : 'No support for the File API in this web browser',
-            'noimage'     : 'Unsupported file type!',
-            'maxsize'     : '500' //Kb
-        };
-        this.each(function(){
-            if(o) $.extend(s, o);
-            var instructions = $('<div>').appendTo($(this));
-            s.init($(this));            
-            if(!$(this).data('value'))
-                instructions.addClass('instructions').html(s.instructions);
 
-            $(this)
-            .bind({
-                dragleave: function (e) {
-                    e.preventDefault();
-                    if($(this).data('value'))
-                        instructions.removeClass().empty();
-                    else
-                        instructions.removeClass('over').html(s.instructions);
-                },
-                dragenter: function (e) {
-                    e.preventDefault();
-                    instructions.addClass('instructions over').html(s.over);
-                },
-                dragover: function (e) {
-                    e.preventDefault();
-                }
-            });
-            this.addEventListener("drop", function (e) {
-                e.preventDefault();
-                s.start($(this));
-                console.log(e.dataTransfer.files);
-                m.traverse(e.dataTransfer.files, $(this));
-                instructions.removeClass().empty();
-            },false);
-        });
+    $.fn.mimedrop = function(method) {
+        if (methods[method]) {
+            return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
+        } else if (typeof method === 'object' || ! method) {
+            return methods.init.apply(this, arguments);
+        } else {
+            $.error('Method ' + method + ' does not exist on jQuery.mimedrop');
+        }
     };
-})( jQuery );
+})(jQuery);
